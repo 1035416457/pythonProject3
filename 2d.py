19c812ad8df6d857464991a4eaa1706d3349e578
think <1035416457@qq.com>
2023-07-24 17:28:26 +0800

    默认的

--- 2d.py
+++ 2d.py
@@ -3,9 +3,9 @@ import base64
 
 def get_answer():
     # 获取redis配置参数
-    auth = base64.b64encode( ("dd970442d9f46b7e07a24fb83d016169").encode( encoding='utf-8' )).decode( encoding="utf-8" )
-    print( auth )
-    return auth
+    auth2 = base64.b64encode( ("dd970442d9f46b7e07a24fb83d016169").encode( encoding='utf-8' )).decode( encoding="utf-8" )
+    print( auth2 )
+    return auth2
 
 
 if __name__ == '__main__':
